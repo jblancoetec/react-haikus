5c8f186dd2459e15e88f1b2f0b7db70bfa489208
Joaquin Blanco <jblanco@etec.uba.ar>
2021-10-27 19:14:08 -0300

    Haikus insertados

--- src/home/Tarjeta.jsx
+++ src/home/Tarjeta.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import "./Tarjeta.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShareAlt } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const urlBaseImagen = "https://picsum.photos";
 
 const Tarjeta = () => {
+  const [haikuVisible, setHaikuVisible] = useState(false);
   return (
     <div className="Tarjeta">
-      <div className="ImagenTarjeta">
+      <div
+        className="ImagenTarjeta"
+        onClick={() => setHaikuVisible(!haikuVisible)}
+      >
+        <div
+          className="ContenedorHaiku"
+          style={{
+            display: haikuVisible ? "block" : "none",
+          }}
+        >
+          este es un haiku muy chiquito <br />
+          para todos aquellos que quieran <br /> verlo
+        </div>
         <img src={`${urlBaseImagen}/500/300`} alt="ImagenHaiku" />
       </div>
       <div className="CuerpoTarjeta">
