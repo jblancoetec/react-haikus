a36b469e87247f45967058c4fffdb6c604460ae5
Joaquin <Jblanco@etec.uba.ar>
2021-11-11 23:14:19 -0300

    Importando haikus

--- src/home/Tarjeta.jsx
+++ src/home/Tarjeta.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 const urlBaseImagen = "https://picsum.photos";
 
-const Tarjeta = () => {
+const Tarjeta = ({ autor, haiku }) => {
   const [haikuVisible, setHaikuVisible] = useState(false);
   return (
     <div className="Tarjeta">
@@ -20,15 +20,14 @@ const Tarjeta = () => {
             display: haikuVisible ? "block" : "none",
           }}
         >
-          este es un haiku muy chiquito <br />
-          para todos aquellos que quieran <br /> verlo
+          {haiku}
         </div>
         <img src={`${urlBaseImagen}/500/300`} alt="ImagenHaiku" />
       </div>
       <div className="CuerpoTarjeta">
         <div className="InfoTarjeta">
           <img src={`${urlBaseImagen}/32/32`} alt="" />
-          <h4>Titulo del Haiku</h4>
+          <h4>{autor}</h4>
         </div>
         <FontAwesomeIcon icon={faShareAlt} className="IconoCompartirTarjeta" />
       </div>
